9dfe6fcff3fb4504eb7ce57c2310d3011ebc6023
BookerVII <26764459+BookerVII@users.noreply.github.com>
2022-02-14 20:18:05 +0100

    Create salads.sh

---
+++ salads.sh
@@ -0,0 +1,33 @@
+Muh Superfood Walk with the Earth Faggot Salad
+
+    Spinach
+    Kale
+    OPTIONAL: Romain, Red Leaf, or Iceberg to “crunch” it up
+    Blueberries
+    Walnuts
+    Chia/Hemp Seeds
+    Wild Rice/Jasmine Rice
+    Craisins
+    Chicken/Tofu
+    Nutritional Yeast
+    Dressing
+
+PEAK BLOATLORD SALAD
+
+    Spinach
+    Kale
+    OPTIONAL (see above)
+    Gold/Russet Potatoes
+    Beans/Lentils
+    Quinoa/Rice
+    OPTIONAL: Chicken/Tofu
+    Nutritional Yeast
+    Dressing
+
+Happy Medium Salad
+
+    Spinach/Kale/Optional
+    Wild/Jasmine Rice
+    Blueberries
+    Brotein
+    Dressing
